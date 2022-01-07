84f38c6e92d7c6be31d48b938d613ad737b59c1e
Sean <seanscott95@live.com>
2022-01-07 17:31:41 +1100

    pseudocode, created main empty functions

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+
+// when you search for a city you get current forecast and 5 day forecast
+// the city searched is added to the histroy and local storage
+// when you click on a city in the search history you get the current forecast and 5 day forecast
+// current forecast will consist of city name, the date, an icon, the temp, the uv index
+// the icon must match the weather conditions
+// the uv index must be coloured to favourable, moderate r severe
+// the 5 day forecast must display the date, icon of weather, the temp, wind speed and humidty
+
+
+
+
+function searchCity() {
+
+}
+
+function currentForecast() {
+    
+}
+
+function fiveDayForecast() {
+
+}
+
+function saveSearchHistory() {
+
+}
+
+// needed??
+function weatherIcon() {
+
+}
+
+// needed??
+function uvIndex() {
+    
+}
