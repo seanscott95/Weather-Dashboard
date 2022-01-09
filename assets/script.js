8971b8f811e1824ccf3e9142055b09f3a70bfb09
Sean <seanscott95@live.com>
2022-01-09 12:33:41 +1100

    tried a different approach to fetch, didnt work

--- assets/script.js
+++ assets/script.js
@@ -49,6 +49,25 @@ async function requestData() {
 requestData();
 
 
+// function getApi(requestUrl) {
+//     fetch(requestUrl)
+//       .then(function (response) {
+//         console.log(response.status);
+//         //  Conditional for the the response.status.
+//         if (response.status !== 200) {
+//           // Place the response.status on the page.
+//           responseText.textContent = response.status;
+//         }
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         // Make sure to look at the response in the console and read how 404 response is structured.
+//         console.log(data);
+//       });
+//   }
+  
+//   getApi(completeUrl);
+  
 
 
 // fetch(url)
