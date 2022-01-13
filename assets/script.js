920a1753a0ed1f4f79beb5e710331df27e27561b
Sean <seanscott95@live.com>
2022-01-13 17:31:22 +1100

    rearranged and added comments to everything in js file

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,5 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-
-// when you search for a city you get current forecast and 5 day forecast
-// the city searched is added to the histroy and local storage
-// when you click on a city in the search history you get the current forecast and 5 day forecast
-// current forecast will consist of city name, the date, an icon, the temp, the uv index
-// the icon must match the weather conditions
-// the uv index must be coloured to favourable, moderate r severe
-// the 5 day forecast must display the date, icon of weather, the temp, wind speed and humidty
-
 // TODO:
-// when click on town list, enter value and show results
-// when town added to list add to local storage
-// have uv index change color depending on level
-
-
-// api key d314e3bf342100cced195fd2b14e5db1
-
-// var url = "http://api.openweathermap.org/data/2.5/onecall?appid=d314e3bf342100cced195fd2b14e5db1&lat=30.00&lon=30.00" //lat=&lon=&appid=   &exclude
+// 
 
 // variables for the current temp elements
 var currentTownEl = document.querySelector(".currentTown");
@@ -36,6 +8,7 @@ var currentTempEl = document.querySelector(".currentTemp");
 var currentHumidityEl = document.querySelector(".currentHumidity");
 var currentWindEl = document.querySelector(".currentWind");
 var currentUvEl = document.querySelector(".currentUv");
+
 // variables for the five day forecast elements
 var firstDayEl = document.querySelector(".firstDay");
 var secondDayEl = document.querySelector(".secondDay");
@@ -43,35 +16,34 @@ var thirdDayEl = document.querySelector(".thirdDay");
 var fourthDayEl = document.querySelector(".fourthDay");
 var fifthDayEl = document.querySelector(".fifthDay");
 
+// Variables for elements in the top row for the current day forecast
 var currentTownNameEl = document.querySelector(".currentTownName");
 var currentTownDateEl = document.querySelector(".currentTownDate");
 var currentTownIconEl = document.querySelector(".currentTownIcon");
 
-
-
+// Variables for the form and list elements
+var listEl = document.querySelector(".list");
 var searchFormEl = document.querySelector(".mainForm");
 var submitBtn = document.querySelector("#submit");
+var cityInput = document.querySelector("#inputCity");
 
+// Variable to change elements background
+var uvAnswerBtn = document.querySelector("#uvAnswer");
+
+// Variables for the api url for fetch
 var apiWeather = "http://api.openweathermap.org/data/2.5/weather?";
 var apiOneCall = "https://api.openweathermap.org/data/2.5/onecall?";
 var lon = "";
 var lat = "";
-
-var cityInput = document.querySelector("#inputCity");
-
 var apiKey = "bbc093470b839c994653daa51c632f1b";
 var apiUnits = "metric";
 var completeUrl = apiWeather + "q=" + cityInput.value + "&appid=" + apiKey + "&units=" + apiUnits;
-console.log(cityInput.value);
-console.log(completeUrl);
 
-var uvAnswerBtn = document.querySelector("#uvAnswer");
 
-// Receives 2 objects of weather data and prints the data to the respective element
+
+// Receives 2 objects of weather data and prints the data to the respective element in the current weather container
 function printResults(firstObj, secondObj) {
-    console.log(firstObj);
-    console.log(secondObj);
-
+    
     if (firstObj.name) {
         currentTownNameEl.textContent = firstObj.name;
     } else {
@@ -111,7 +83,7 @@ function printResults(firstObj, secondObj) {
     if (secondObj.daily[0].uvi) {
         uvAnswerBtn.textContent = secondObj.daily[0].uvi;
         // console.log(secondObj.daily[0].uvi);
-        uvIndex(secondObj.daily[0].uvi, uvAnswerBtn);             // uvIndex function not working
+        uvIndex(secondObj.daily[0].uvi, uvAnswerBtn);
     } else {
         currentUvEl.textContent += "N/A";
     }
@@ -145,31 +117,28 @@ function printResults(firstObj, secondObj) {
     }
 }
 
- //display date     dt
-    //display icon     weather.icon
-    //display temp     temp.day
-    //dsiplay windspeed    wind_speed
-    //display humidity     humidity
-
-// Creates the five day weather forecast card and applies the respective data to the respective element
+// Creates the five day weather forecast card and prints the respective data to the respective element
 function printFiveDayCard(dayData, dayElement) {
     var dateCard = document.createElement("div");
     dateCard.className = "fiveDayDate";
+
     var convertedDate = new Date(dayData.dt*1000).toLocaleDateString("en-AU");
     dateCard.textContent = convertedDate;
+
     var iconCard = document.createElement("img");
-    iconCard.src = getFullIconUrl(dayData.weather[0].icon);                               // weather icon not working
+    iconCard.src = getFullIconUrl(dayData.weather[0].icon);
+
     var tempCard = document.createElement("div");
     tempCard.textContent = "Temp: " + dayData.temp.day + "C";
+
     var windCard = document.createElement("div");
     windCard.textContent = "Wind: " + dayData.wind_speed + "km/h";
+
     var humidityCard = document.createElement("div");
     humidityCard.textContent = "Humidity: " + dayData.humidity + "%";
 
     dayElement.textContent = "";
     dayElement.append(dateCard, iconCard, tempCard, windCard, humidityCard);
-
-    //create element add info to element, add element to element
 }
 
 // Requests data from the weather api and prints those results to a function to be used
@@ -196,7 +165,7 @@ async function requestData(query) {
     printResults(data, secondData);
 }
 
-//
+// Creates a new list item if value isnt in local storage already
 function appendSearch(val) {
     while (!localStorage.getItem(val)) {
         var ul = document.querySelector(".list");
@@ -207,31 +176,44 @@ function appendSearch(val) {
     }
 }
 
-// 
+// Uses city as values to send through appendSearch and requestData functions
 function searchCity(city) {
     appendSearch(city);
     requestData(city);
 }
 
-
-searchFormEl.addEventListener("submit", function(event) {
+// 
+function listSearch(event) {
     event.preventDefault();
-    cityInputVal = cityInput.value;
-    console.log(cityInputVal);
-    searchCity(cityInputVal);
-})
+    console.log(event.target.textContent);
+    var listName = event.target.textContent;
+    console.log(listName);
+    searchCity(listName);
+}
 
-
-
+// Returns the correct url for each image weather icon
 function getFullIconUrl(icon) {
     return "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 }
 
-
+// Creates a local stroage key and value as the towns name entered 
 function appendToLocal(townName) {
     localStorage.setItem(townName, townName);
 }
 
+// Changes the background of the UV index depending on level of UV
+function uvIndex(level, element) {
+    console.log(level);
+    if (level <= 2) {
+        element.style.backgroundColor = "green";
+    } else if (level > 2 && level <= 5) {
+        element.style.backgroundColor = "yellow";
+    } else if (level > 5 && level <= 7) {
+        element.style.backgroundColor = "orange";
+    } else {element.style.backgroundColor = "red"}
+}
+
+// Sets towns in list in local storage so they cant be added again
 function checkLocal() {
     localStorage.setItem("Melbourne", "Melbourne");
     localStorage.setItem("Brisbane", "Brisbane");
@@ -243,30 +225,15 @@ function checkLocal() {
     localStorage.setItem("Perth", "Perth");
 }
 
+// Runs checkLocal
 checkLocal();
 
-
-var listEl = document.querySelector(".list");
+// Event listeners
 listEl.addEventListener("click", listSearch);
 
-function listSearch(event) {
+searchFormEl.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log(event.target.textContent);
-    var listName = event.target.textContent;
-    console.log(listName);
-    searchCity(listName);
-}
-
-
-// not working
-
-function uvIndex(level, element) {
-    console.log(level);
-    if (level <= 2) {
-        element.style.backgroundColor = "green";
-    } else if (level > 2 && level <= 5) {
-        element.style.backgroundColor = "yellow";
-    } else if (level > 5 && level <= 7) {
-        element.style.backgroundColor = "orange";
-    } else {element.style.backgroundColor = "red"}
-}
+    cityInputVal = cityInput.value;
+    console.log(cityInputVal);
+    searchCity(cityInputVal);
+})
